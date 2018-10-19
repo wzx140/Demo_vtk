e8be24bc2761ccba734d4071539c50b87a03a090
wzx140 <masterwangzx@gmai.com>
2018-10-19 12:16:48 +0800

    init;

---
+++ src/mainwindow.cpp
@@ -0,0 +1,38 @@
+#include <vtkCylinderSource.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkGenericOpenGLRenderWindow.h>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+        QMainWindow(parent),
+        ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+    auto cylinderSource = vtkSmartPointer<vtkCylinderSource>::New();
+    cylinderSource->SetCenter(0.0, 0.0, 0.0);
+    cylinderSource->SetRadius(5.0);
+    cylinderSource->SetHeight(7.0);
+    cylinderSource->SetResolution(100);
+
+    auto mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    mapper->SetInputConnection(cylinderSource->GetOutputPort());
+
+    auto actor = vtkSmartPointer<vtkActor>::New();
+    actor->SetMapper(mapper);
+
+    auto renderer = vtkSmartPointer<vtkRenderer>::New();
+    renderer->AddActor(actor);
+
+    auto window = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
+    window->AddRenderer(renderer);
+
+    ui->openGLWidget->SetRenderWindow(window);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
